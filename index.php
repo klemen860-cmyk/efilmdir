abf8775cb49637884efb3e192b2f9e2208999aa1
klemen860-cmyk <klemen860@gmail.com>
2026-01-05 05:12:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
+// Tüm Player'lara ve tarayıcılara izin ver
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if(isset($_GET['id'])){
-    header("Location: " . $_GET['id'], true, 302);
+    $url = $_GET['id'];
+    // Player'ların videoyu görmesi için yönlendirmeyi yap
+    header("Location: " . $url, true, 301); 
     exit;
 } else {
-    echo "Sistem Hazir!";
+    echo "Sistem Hazir! Player linkini bekliyor.";
 }
 ?>
